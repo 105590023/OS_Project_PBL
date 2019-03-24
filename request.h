5893e2aff1298c56afaa08fea1ac19e9335e18ca
105590026 <105590026@ntut.edu.tw>
2019-03-24 17:41:21 +0800

    request

---
+++ request.h
@@ -0,0 +1,22 @@
+#pragma once
+#ifndef REQUEST_H
+#define REQUEST_H
+
+#include "abstract_request.h"
+
+#include <string>
+
+using namespace std;
+
+typedef void (*logFunction)(string test);
+
+class Request :Public Abstract_Request {
+  int value;
+  logFunction outFnc;
+  public :
+  void setValue(int value) {this->value = value;}
+  void setOutput (logFunction fnc) {outFnc = fnc;}
+  void process();
+  void finish();
+};
+#endif
